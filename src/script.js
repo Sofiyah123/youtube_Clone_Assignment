1757cc0310c321a6716bac7229492204592293e1
Sofiyah123 <musasophiat@gmail.com>
2024-03-07 07:03:44 -0800

    Added the cloned Youtube

---
+++ src/script.js
@@ -0,0 +1,5 @@
+let menuIcon = document.querySelector('.menuIcon');
+let sidebar = document.querySelector('.sidebar');
+menuIcon.onclick = function(){
+    sidebar.classList.toggle("small-sidebar");
+}
